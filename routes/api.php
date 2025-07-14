b7e7382457ed41cd1f758702c6a11f3c3b1173b7
salmanabdurrahman <salmanabdurrahman12345@gmail.com>
2025-07-14 12:50:33 +0700

    feat: implement authentication and authorization for api v1
    - Implemented authentication using sanctum for api version 1.
    - Added login and logout routes for user authentication.
    - Protected routes with sanctum authentication middleware.
    - Created division and employee resource controllers with index, store, show, update, and destroy methods.
    - Added division routes to retrieve division data.
    - Added employee routes to manage employee resources.

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,22 @@
 <?php
 
+use App\Http\Controllers\Api\V1\AuthController;
+use App\Http\Controllers\Api\V1\DivisionController;
+use App\Http\Controllers\Api\V1\EmployeeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('v1')->group(function () {
+    Route::middleware('guest:sanctum')->group(function () {
+        Route::post('/login', [AuthController::class, 'login']);
+    });
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
+        Route::get('/divisions', [DivisionController::class, 'index']);
+        Route::apiResource('/employees', EmployeeController::class);
+    });
+});
